8521bf3462f7b6ea1bd9ca4685ffc5e8efdca49d
omar lammas <omarlammas@gmail.com>
2025-04-05 11:30:45 +0000

    fix import error

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,10 +1,10 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
-import Username from "../features/user/Username";
+import Username from "../features/user/UserName";
 
 function Header() {
   return (
-    <header className="flex w-full items-center mx-auto justify-between border-b border-stone-200 bg-yellow-50 p-4 uppercase sm:px-6">
+    <header className="mx-auto flex w-full items-center justify-between border-b border-stone-200 bg-yellow-50 p-4 uppercase sm:px-6">
       <Link
         to="/"
         class="self-center whitespace-nowrap text-2xl font-semibold text-stone-600"
